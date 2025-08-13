76e5365414426da7db18d92a0184bf6aac14ba8c
Ademir de Lucca Junior <ademirdeljr@outlook.com>
2025-08-13 19:49:54 -0300

    Delete TP1DR2/ex6.py

--- TP1DR2/ex6.py
+++
@@ -1,7 +0,0 @@
-nome = "Ademir"              
-sobrenome = "Junior"    
-turma = "3745"                 
-
-mensagem = "Bem-vindo(a), " + nome + " " + sobrenome + "! Sua turma é " + turma + "."
-
-print(mensagem)
