041a27b833cea697f937845d4815bd4b20076e81
Ademir de Lucca Junior <ademirdeljr@outlook.com>
2025-08-13 19:49:41 -0300

    Delete TP1DR2/ex4.py

--- TP1DR2/ex4.py
+++
@@ -1,9 +0,0 @@
-ano_2_digitos = 25            
-tempo_minutos = 150 + ano_2_digitos 
-tempo_horas = 2.25         
-
-horas_equivalentes = tempo_minutos / 60  
-minutos_equivalentes = tempo_horas * 60  
-
-print("Horas equivalentes a", tempo_minutos, "minutos:", round(horas_equivalentes, 2))
-print("Minutos equivalentes a", tempo_horas, "horas:", round(minutos_equivalentes, 2))
