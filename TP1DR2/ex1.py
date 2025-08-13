88cf081029f3b3f82372e6e163728fa2f468bd5f
Ademir de Lucca Junior <ademirdeljr@outlook.com>
2025-08-13 19:48:51 -0300

    Delete TP1DR2/ex1.py

--- TP1DR2/ex1.py
+++
@@ -1,9 +0,0 @@
-nome_estagiario = "Ademir de Luca Junior"  
-idade_estagiario = 21        
-codigo_departamento = "2104"   
-
-
-print("Cadastro de Estagiário:")
-print("Nome:", nome_estagiario)
-print("Idade:", idade_estagiario)
-print("Código do Departamento:", codigo_departamento)
